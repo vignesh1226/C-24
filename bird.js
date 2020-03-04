22a82e660c60fa4c13c16531f4e6891cf2abbf08
vignesh1226 <61319256+vignesh1226@users.noreply.github.com>
2020-03-04 19:06:40 +0530

    angry bird stage 1

---
+++ bird.js
@@ -0,0 +1,29 @@
+class Bird {
+    constructor(x,y) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y,20,20, options);
+      this.width = 20;
+      this.height = 20;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      pos.x = mouseX;
+      pos.y = mouseY;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      fill(255);
+      strokeWeight(4);
+      stroke("red");
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
